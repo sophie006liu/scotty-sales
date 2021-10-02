38d5341de2de94db3f8f326988d08164dbfb00e1
Michelle Li <mli6@andrew.cmu.edu>
2021-10-02 15:57:11 -0400

    fix

--- src/Home.js
+++ src/Home.js
@@ -4,28 +4,11 @@ import useFetch from './useFetch';
 
 const Home = () => {
     const {data: marketables, isPending, error} = useFetch('http://localhost:8000/marketables')
-    const [filt, setFilt] = useState('');
+
 
     const shopLink = "http://www.google.com"
     const loginLink = "http://www.google.com"
 
-        return (
-          <div className = "home">
-              {error && <div>{error}</div>}
-              {isPending && <div>Loading...</div>}
-              <h1>All items</h1>
-              <select
-                  value={filt}
-                  onChange={(e) => console.log((e.target.value))}>
-                  <option value="">Filter by:</option>
-                  <option value="Low">Price: Low to High</option>
-                  <option value="High">Price: High to Low</option>
-                  <option value="Kitchen">Category: Kitchen</option>
-                  <option value="Bedroom">Category: Bedroom</option>
-                  <option value="Living Spaces">Category: Living Spaces</option>
-                  <option value="Miscellaneous">Category: Miscellaneous</option>
-              </select>
-              {marketables && <MarketList marketables = {marketables} filt = {filt}/> }
 
       return (
         
