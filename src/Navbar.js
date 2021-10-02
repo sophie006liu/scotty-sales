570b98efb5cc3382dd998841a54856c6bc9ff97a
Michelle Li <mli6@andrew.cmu.edu>
2021-10-02 16:41:14 -0400

    cosmetic changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,6 @@ const Navbar = () => {
         <nav className = "navbar">
             <Link to ="/Home"><h1> CMU MarketPlace </h1></Link>
             <div className = "links">
-            <Link to = "/Home"> Home </Link> 
             <Link to = "/BlocksHome"> Blocks </Link>
             <Link to = "/Items"> Items </Link> 
             <Link to ="/CreateItem">Create Listing</Link>
