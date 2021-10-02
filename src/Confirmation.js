dfd77cdde35a1b3ac6c11a4bace412a1d2c94fc4
abbyli437 <31529490+abbyli437@users.noreply.github.com>
2021-10-02 11:51:18 -0400

    added button details in confirm to css

--- src/Confirmation.js
+++ src/Confirmation.js
@@ -15,26 +15,10 @@ const Confirmation = () => {
         <div className="confirmation">
             <h2>Success! You have added a new listing</h2>
             <div>
-                <button name="addListing" onClick={clickAddItem} 
-                style= {{
-                    color: 'white',
-                    backgroundColor: '#f1356d',
-                    borderRadius: '8px',
-                    marginTop: '20px',
-                    marginRight: '20px',
-                    boxShadow: 'none'
-                }}>
+                <button name="addListing" onClick={clickAddItem} >
                     Make another listing
                 </button>
-                <button name="Home" onClick={clickGoHome}
-                style= {{
-                    color: 'white',
-                    backgroundColor: '#f1356d',
-                    borderRadius: '8px',
-                    marginTop: '20px',
-                    marginRight: '20px',
-                    outline: 'none'
-                }}>
+                <button name="Home" onClick={clickGoHome}>
                     Home
                 </button>
             </div>
