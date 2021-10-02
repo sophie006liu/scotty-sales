f7009d41dd8bdba52c5d1ce51e0edcf116ff991a
sophie006liu <41531009+sophie006liu@users.noreply.github.com>
2021-10-02 09:42:47 -0400

    +deleting capabilities

--- src/MarketDetails.js
+++ src/MarketDetails.js
@@ -1,20 +1,31 @@
 import {useParams} from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const MarketDetails = () => {
     const {id} = useParams()
     const {data: marketable, error, isPending } = 
         useFetch('http://localhost:8000/marketables/'+ id)
+    const history = useHistory();
+
+    const handleClick = () => {
+      fetch('http://localhost:8000/marketables/' + marketable.id, {
+        method: 'DELETE'
+      }).then(() => {
+        history.push('/');
+      }) 
+    }
 
     return ( 
         <div className =  "market-details">
        { isPending && <div>Loading...</div> }
-      { error && <div>{ error }</div> }
-      { marketable && (
+       { error && <div>{ error }</div> }
+       { marketable && (
         <article>
           <h2>{ marketable.title }</h2>
           <p>Posted by { marketable.author }</p>
           <div>{ marketable.body }</div>
+          <button onClick={handleClick}> Purchase this item </button>
         </article>
       )}
         </div>
