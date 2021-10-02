5114a5876b19832dca6ec092290b0c683b867e04
Michelle Li <mli6@andrew.cmu.edu>
2021-10-02 17:23:20 -0400

    style changes home page and images

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,7 @@ const Home = () => {
       rel="stylesheet"
     />
           <div className = "home">
+            <div class="header">
             <div class= "container">
               <div class="row">
                 <div class="col-2">
@@ -27,6 +28,7 @@ const Home = () => {
                   </a>
                   <a href="/BlocksHome" class="btn">Block Exchange &#8594;</a>
                 </div>
+              </div>
               </div>
             </div>
             <div class="categories">
